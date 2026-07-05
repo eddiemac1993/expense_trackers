4367d56bfa035115a6604194ca14df003e26fcb1
eddiemac1993 <manjoloe800@gmail.com>
2026-07-05 05:46:27 +0000

    new

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool("DJANGO_DEBUG", True)
 
-ALLOWED_HOSTS = env_list(
-    "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-)
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
